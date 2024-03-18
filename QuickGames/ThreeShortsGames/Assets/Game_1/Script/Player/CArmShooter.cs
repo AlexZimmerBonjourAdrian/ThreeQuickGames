82518b825e5c42021baba768beea9bab26db9fdd
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2024-03-17 23:45:03 -0300

    Rotation arm for looking mouse, and Start Shoot System

--- QuickGames/ThreeShortsGames/Assets/Game_1/Script/Player/CArmShooter.cs
+++ QuickGames/ThreeShortsGames/Assets/Game_1/Script/Player/CArmShooter.cs
@@ -1,28 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Jobs;
 
 public class CArmShooter : MonoBehaviour
 {
-    [SerializeField]private Transform _ArmWeaponTrasnform;
-    [SerializeField] private Transform _AimTrasnform;
-    [SerializeField]private Vector2 _LookRotation;
 
-    [SerializeField] private float _Speed = 3f;
+    private Camera mainCam;
+    public Vector3 mousePos;
 
-     
-   
-
+    
+    [SerializeField]
+    private InputActionReference pointerPosition;
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+      //  Weapon= GameObject.FindGameObjectWithTag("Weapon").GetComponent<Game>();
+}
     void Update()
     {
-        RotationArmLookAim();
+       //mousePos = pointerPosition.action.ReadValue<Vector2>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 rotation = mousePos - transform.position;
+
+        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotz);
     }
-    public void RotationArmLookAim()
-    {
-      
-        //_ArmWeaponTrasnform.rotation = Quaternion.Slerp(_ArmWeaponTrasnform.rotation, _AimTrasnform.position, _Speed * Time.deltaTime);
-    }
+
 
 }
--- QuickGames/ThreeShortsGames/Assets/Game_1/Script/Player/CShootSystem.cs
+++ QuickGames/ThreeShortsGames/Assets/Game_1/Script/Player/CShootSystem.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class CShootSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    protected int Ammo;
+    protected int AmmoInMag;
+    protected bool IsShoot;
+    protected bool CanReload;
+    protected int MultiplyWeapon;
+
+    //Dispara el arma
+    protected virtual void Fire()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //Recarga el arma
+    protected virtual void Reload()
     {
-        
+
+    }
+    
+    //No poseo municion
+    protected virtual void IsEmpty()
+    {
+
+    }
+
+    //Tengo toda la municion?
+    protected virtual void IsFull()
+    {
+
+    }
+
+    //Tengo municion de reserva
+    protected virtual void IsHaveAmmo()
+    {
+
+    }
+
+    //Descueta la municion al momento de recargar y chequea si se puede recargar y si la municion que queda es correcta
+    protected virtual void RecalculatedAmmo()
+    {
+
     }
 }
+
+
+
